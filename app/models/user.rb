b24b1941729cdb5dbafd7117a988538ef92b8e2a
Dale Palmer and Joey Sandine <jasandine@gmail.com>
2015-03-25 10:45:16 -0700

    Validate user columns, specs passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :listings
   validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
   describe User do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
     it { should have_many :listings }
 end
