1e66df035d4b079237ed675bf4827ba534a6ad29
Dale Palmer and Joey Sandine <jasandine@gmail.com>
2015-03-26 10:29:11 -0700

    Validate presence of user_id and listing_id within reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :listing
+  validates :user_id, :presence => true
+  validates :listing_id, :presence => true
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
   describe Reservation do
     it { should belong_to :user }
     it { should belong_to :listing }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :listing_id }
   end
