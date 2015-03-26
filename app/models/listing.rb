90dc8d9bdbe5e7796bfc7d752cec67b9b9ad9c6c
Dale Palmer and Joey Sandine <dap38@zips.uakron.edu>
2015-03-26 10:25:39 -0700

    Create relation of reservations to user and listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+  has_many :reservations
   validates :title, :presence => true
   validates :price, :presence => true
   validates :date_available, :presence => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :listings
+  has_many :reservations
   validates :email, :presence => true
   validates :password, :presence => true
   validates :password_confirmation, :presence => true
